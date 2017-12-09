979845ff53732d4cc616dd2df2fadbe0ad7868ef
matukai <matt.tanji@gmail.com>
2017-12-08 14:22:31 -1000

    9

--- js/app.js
+++ js/app.js
@@ -58,14 +58,40 @@ function showPrice(){
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
+document.getElementById("Benjamin").addEventListener("click",event);
+
+function event(){
+	document.getElementById("displayQuote").innerHTML = myQuote;
+}
 
 //8. Say It again, Randomly
 /*Create a function that will generate a random quote from the variable below after clicking on the button.*/
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+document.getElementById("random").addEventListener("click",random)
+
+function random(){
+	document.getElementById("displayQuotes").innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+console.log([Math.floor(Math.random() * quotes.length)]);
 
 
 //9. Unlock the Secret to Financial Freedom
-/*Create an event listener that will show and hide the message when clickig on the button.
+/*Create an event listener that will show and hide the message when clicking on the button.
 */
+
+document.getElementById("showHide").addEventListener("click",showMe);
+
+var money = document.getElementById("showmoney");
+
+function showMe(){
+	if(money.style.display === "none"){
+		money.style.display === "block"
+	}else{
+		money.style.display === "display";
+	}
+}
+
+
